3bfba75447e51e8afa269aa2514134d59538a499
xavier-delfini <xavier.delfini@laplateforme.io>
2022-10-21 11:31:05 +0200

    Amélioration du job 9

--- Job9/ftpuser.sh
+++ Job9/ftpuser.sh
@@ -2,20 +2,24 @@ i=$(cat ftpuser.csv | wc -l)
 for h in $(eval echo "{2..$i..1}");
 	do
 	recup[$h]=$(sed -n "$h p" ftpuser.csv)
-	for k in {1..5..1}
+	for k in {1..3..1}
 	do
 		infos[$k]=$(echo ${recup[$h]} | cut -d ',' -f$k)
 		infos[$k]=$(echo ${infos[$k]} | cut -d ' ' -f$k)
 	done
-	user[$h]=${infos[2]}"-"${infos[3]}
-	case ${infos[5]} in
-		Admin*)
-			sudo useradd -m -u ${infos[1]} -p ${infos[4]} --gid root ${user[$h]}
-			echo "L'utilisateur ${user[$h]}a été crée et est passé root"
+	case ${infos[3]} in
+		Sudo*)
+			sudo useradd -m ${infos[1]}
+			sudo usermod -aG sudo ${infos[1]}
+			sudo adduser ${infos[1]} ftp
+			echo "${infos[1]}:${infos[2]}" | sudo chpasswd
+			echo "L'utilisateur ${infos[1]} a été crée et ajouté aux groupes sudo et ftp"
 			;;
 		*)
-			sudo useradd -m -u ${infos[1]} -p ${infos[4]} ${user[$h]}
-			echo "L'utilisateur ${user[$h]}  a été crée et est passé simple utilisateur"
+			sudo useradd -m ${infos[1]}
+			sudo adduser ${infos[1]} ftp
+			echo "${infos[1]}:${infos[2]}" | sudo chpasswd
+			echo "L'utilisateur ${infos[1]} a été crée et ajouté au groupe ftp"
 		;;
 	esac
 done
